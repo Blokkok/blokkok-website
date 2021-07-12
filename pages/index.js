7f5119dd167598d4542531706dbd1641c408cd7e
TheClashFruit <clash.fruit123@gmail.com>
2021-07-12 21:47:14 +0200

    fix: fix "Error: No duplicate props allowed"

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,7 @@ export default function Home() {
 
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleDrawerClose} className={clsx(classes.menuButton, close && classes.hide)}>
+          <IconButton edge="start" className={clsx(classes.menuButton, close && classes.hide)} color="inherit" aria-label="menu" onClick={handleDrawerClose}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
