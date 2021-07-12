6f69ce4fdd416c8358e2bac171390c558c837f9f
TheClashFruit <clash.fruit123@gmail.com>
2021-07-12 20:09:38 +0200

    feat: setup next.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Button from '@material-ui/core/Button';
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+    </div>
+  )
+}
