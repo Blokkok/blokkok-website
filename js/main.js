816699c1759e6eedb50e78b8b1916c042d552493
TheClashFruit <clash.fruit123@gmail.com>
2021-07-30 16:16:15 +0200

    feat: content, contact form

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-let navStatus = false;
+let navStatus  = false;
+let isSendable = true;
 
 const $ = (_querySelector) => { return document.querySelector(_querySelector); }
 
@@ -15,6 +16,49 @@ const BlokkokWebApp = {
       $('#toggle-icon').innerHTML = 'menu';
       $('.navBarCollapse').setAttribute('style', '');
     }
+  },
+  sendMail: () => {
+    if(!isSendable) {
+      alert('Please wait a bit before sending an new one.')
+  
+      return;
+    }
+  
+    isSendable = false;
+  
+    db.collection("contact").doc().set({
+      subject: $('#formControlSubject').value,
+      message: $('#formControlMessage').value,
+      email: $('#formControlEmail').value
+    })
+    .then(() => {
+      const request = new XMLHttpRequest();
+      const params = {
+        "content": null,
+        "embeds": [
+          {
+            "title": $('#formControlSubject').value,
+            "description": $('#formControlMessage').value,
+            "color": 43608,
+            "footer": {
+              "text": $('#formControlEmail').value
+            },
+            "timestamp": new Date()
+          }
+        ]
+      }
+  
+      request.open("POST", "https://canary.discord.com/api/webhooks/869942680241725490/JkXa24e5OS9hHi1N749OvQ3WTC0yjn02rxCuWWVspcM-ol6TVOfd_Wxu8xl92kEEqqVL");
+      request.setRequestHeader('Content-type', 'application/json');
+      request.send(JSON.stringify(params));
+    })
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    });
+  
+    setTimeout(() => {
+      isSendable = true;
+    }, 10000);
   }
 }
 
